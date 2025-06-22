054bd8e7b02b1cea911209d34fa53f4635de989c
Omar Zaida <omario-555@hotmail.fr>
2025-06-22 21:53:47 +0100

    better lane detection, obstacle spawn in correct lane, addec pick ups

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,14 +1,23 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
     [SerializeField] GameObject FencePrefab;
+    [SerializeField] GameObject HealthPrefab;
+    [SerializeField] GameObject CoinPrefab;
     [SerializeField] float[] Lanes = { -2.5f,0,2.5f };
-    [SerializeField] int safeLane;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float appleSpawnChace = 0.3f;
+    [SerializeField] float CoinSpawnChance = 0.5f;
+    [SerializeField] int maxCoins = 10;
+    bool ChunkIsOccupied = false;
+    int safeLane = 1;// middle lane is the safe lane by default
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
     {
         SpawnFence();
+        SpawnApple();
+        SpawnCoins();
     }
 
     // Update is called once per frame
@@ -18,18 +27,58 @@ public class Chunk : MonoBehaviour
     }
 
 
-    void SpawnFence()
+    private void SpawnFence()
     {
-        safeLane = Mathf.RoundToInt(Random.Range(0, Lanes.Length));//choose a random safe lane
+        safeLane = Random.Range(0, Lanes.Length);//choose a random safe lane
+        int spawnOneOrNoFence = Random.Range(0, 3);// Get a Random Number of fence 1,2 or none
+
+        int FenceCount = 0;//How many fences we Instantiated
 
         for (int i = 0; i < Lanes.Length; i++)
         {
 
-            if (i == safeLane)
+            if (i == safeLane)// dont spawn a fence if it s the designated safe lane
                 continue;
 
-            Vector3 coords = new Vector3(Lanes[i], transform.position.y, transform.position.z);
-            Instantiate(FencePrefab, coords,Quaternion.identity,transform);
+            InstantiatePrefab(i, FencePrefab);
+            FenceCount++;
+
+            if (FenceCount == spawnOneOrNoFence)// If we are the max number of fences then we can safely leave the loop
+                break;
         }
     }
+    private void SpawnApple()
+    {
+        if (Random.value > appleSpawnChace)
+            return;
+        InstantiatePrefab(safeLane, HealthPrefab);
+        ChunkIsOccupied = true;
+    }
+
+    private void SpawnCoins()
+    {
+        if (Random.value > CoinSpawnChance || ChunkIsOccupied) // if we spawn an apple then dont spawn coins
+            return;
+
+        int zOffset = -3;
+        for (int i = 0; i < Random.Range(0,maxCoins); i++)
+        {
+            zOffset++; ;
+            InstantiatePrefab(safeLane, CoinPrefab,zOffset);
+        }
+    }
+
+
+    private void InstantiatePrefab(int laneIndex,GameObject Prefab,float zOffset = 0)
+    {
+        Vector3 coords = new Vector3(Lanes[laneIndex], transform.position.y, transform.position.z+zOffset);
+        Instantiate(Prefab, coords, Quaternion.identity, transform);
+    }
+
+  
+
+
+
 }
+
+
--- Assets/Scripts/Obstacle Spawner.cs
+++ Assets/Scripts/Obstacle Spawner.cs
@@ -8,6 +8,16 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] float SpawnInterval;
     [SerializeField] int NumberOfObstacle;
     [SerializeField] float XRange = 3f;
+    [SerializeField] float[] Lanes = { -2f, 0, 2f };
+    Transform player;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,14 +26,33 @@ public class ObstacleSpawner : MonoBehaviour
 
     // Update is called once per frame
     IEnumerator SpawnObstacle() 
-    {   
+    {
+      
+
         while (true)
         {
+            SpawnLocationFromPlayerPosition();
 
             int Index = Random.Range(0, ObstaclePrefabs.Length);
-            SpawnLocation.x = Random.Range(-XRange, XRange);
             Instantiate(ObstaclePrefabs[Index], SpawnLocation, Random.rotation, transform);
             yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+
+    private void SpawnLocationFromPlayerPosition()
+    {
+        Vector3 playerPosition = player.position;
+        if (playerPosition.x < -1)
+        {
+            SpawnLocation.x = Lanes[0];
+        }
+        else if (playerPosition.x < 1)
+        {
+            SpawnLocation.x = Lanes[1];
+        }
+        else
+        {
+            SpawnLocation.x = Lanes[2];
         }
     }
 }
